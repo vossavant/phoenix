348dd945feb51a653c67862aa2ddaa75b9bc488a
vossavant <ryan@gospotcheck.com>
2016-05-31 11:05:51 -0600

    Add Material Icon to master search form

--- wp-content/themes/quoteboard/searchform.php
+++ wp-content/themes/quoteboard/searchform.php
@@ -8,5 +8,5 @@
 <form action="<?= esc_url( home_url( '/' ) ); ?>" class="search-form" id="site-search" method="get" role="search">
 	<label class="hidden" for="s">Search Quoteboard</label>
 	<input id="s" name="s" placeholder="Find something new" type="text" />
-	<button class="ico search no-ajax" type="submit">Search</button>
+	<button class="no-ajax" type="submit">Search <i class="material-icons">search</i></button>
 </form>
